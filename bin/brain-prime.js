3ff89ad7212be11d23c4bb2f3463e91ad3aa73c2
camilla228 <kamilla.musabekova@mail.ru>
2024-02-14 17:31:02 +0300

    step 9.1

---
+++ bin/brain-prime.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+import gameBody from '../src/index.js';
+
+const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const task = () => {
+    const num = Math.floor(Math.random() * 100);
+
+    let answer = '';
+ 
+};
--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -4,15 +4,19 @@ import gameBody from '../src/index.js';
 const description = 'What number is missing in the progression?';
 
 const task = () => {
-  const arraylength = Math.floor(Math.random() * 5) + 5; // случайная длина массива
+  const arrayLength = Math.floor(Math.random() * 5) + 5; // случайная длина массива
   const progressDelta = Math.round(Math.random() * 10); // шаг прогрессии
-  const firstNum = Math.round(Math.random() * 100); // начальное число в прогрессии
+  const firstNum = Math.round(Math.random() * 100); // начальное число прогрессии
   const progression = [];
   progression.push(firstNum);
-  for (let i = 1; i < arraylength; i += 1) {
+  for (let i = 1; i < arrayLength; i += 1) {
     progression.push(progression[i - 1] + progressDelta); // наполняем массив прогрессии
   }
-  
+  const emptyIndex = Math.floor(Math.random() * arrayLength); // индекс числа, которое мы выкинем
+  const answer = `${progression[emptyIndex]}`;
+  progression[emptyIndex] = '..';
+
+  return [progression.join(' '), answer];
 };
 
-task();
+gameBody(description, task);
